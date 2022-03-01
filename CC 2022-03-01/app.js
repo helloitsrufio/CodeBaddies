b3642f9e1eaa29ca8140a1f77cc45ad661cd373f
Ruth Reed <ruthreed.dev@gmail.com>
2022-03-01 16:05:04 -0700

    Add coding challenge attempt for 3-1-22.

--- CC 2022-03-01/app.js
+++ CC 2022-03-01/app.js
@@ -3,3 +3,18 @@
 // Array can contain numbers or strings. X can be either.
 
 // Return true if the array contains the value, false if not.
+
+//P: Given array A, and rando value X. Want to see if A has X in it. A can have numbers, strings. X can be a number or a string. Is it ever empty? Undefined? What do we do about special characters?
+//R: True if x is in a, false if x is not. BUT TRUE, FALSE (boolean)
+//E: Given [1,2,3] as A and 4 as X, should return FALSE
+//   Given [7,6,3,2] as A and 6 as X, should return TRUE
+//   Given [] as A and 5 as X, should return FALSE
+//P: Make a function that takes in two parameters: an array, and a rando number.
+function numInArray(arr,num){
+    //look through the array and see if there are any elements that match num. 
+    console.log(arr.map(elem=>elem === num ? true : false))
+    //conditional: if they do, return TRUE, else, return FALSE
+}
+
+numInArray([1,2,3],4) //should return false
+numInArray([7,6,3,2],6) //should return true
